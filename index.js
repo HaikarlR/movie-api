746f8fe8d825af34bab62be7d787fd4b16d7a82b
HaikarlR <122443850+HaikarlR@users.noreply.github.com>
2023-06-27 11:48:00 +1000

    Created schema for MongoDB, Updated URL endpoints  using Mongoose
    - Created models.js for movies and users schema
    - Updated documentation

--- index.js
+++ index.js
@@ -6,160 +6,24 @@ const express = require('express'),
     uuid = require('uuid');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
     // create a write stream (in append mode)
     // a 'log.txt' file is created in root directory
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'});
+
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 // setup the logger
 app.use(morgan('combined', {stream: accessLogStream}));
-app.use(bodyParser.json());
 
-let users = [
-    { 
-        id: 1,
-        name: 'Ken',
-        favouriteMovies: []
-    },
-    {
-        id: 2,
-        name: 'Cam',
-        favouriteMovies:['Paddington']
-    },
-]
-
-let movies = [
-    {
-        Title: 'Spirited Away',
-        Director: { 
-            Name: 'Hayao Miyazaki',
-            Bio: 'A co-founder of Studio Ghibli, he has attained international acclaim as a masterful storyteller and creator of Japanese animated feature films, and is widely regarded as one of the most accomplished filmmakers in the history of animation.',
-            Birthyear: '1941',
-            Deathyear: 'none'
-        },
-        Genre: { 
-            Name:'Fantasy',
-            Description: 'speculative fiction involving magical elements, typically set in a fictional universe and sometimes inspired by mythology and folklore.' 
-        }
-    },
-    {
-        Title: 'Fantastic Mr. Fox',
-        Director: {
-            Name: 'Wes Anderson',
-            Bio: 'Wesley Wales Anderson is an American filmmaker. His films are known for their eccentricity, unique visual and narrative styles, and frequent use of ensemble casts. They often contain themes of grief, loss of innocence, and dysfunctional families.',
-            Birthyear: '1969',
-            Deathyear: 'None'
-        },
-        Genre: { 
-            Name: 'Comedy',
-            Description:'Fiction that consists of discourses or works intended to be humorous or amusing.'
-        }
-    },
-    {
-        Title: 'Fury',
-        Director: { 
-            Name:'David Ayer',
-            Bio: 'An American filmmaker known for making crime films that are set in Los Angeles and deal with gangs and police corruption. ',
-            Birthyear: '1968',
-            Deathyear: 'none'
-        },
-        Genre: { 
-            Name: 'War',
-            Description: 'War film set in times of war.'  }
-    },
-    {
-        Title: 'Southpaw',
-        Director: { 
-            Name:'Antoine fuqua',
-            Bio: 'An American filmmaker, known for his work in the action and thriller genres.',
-            Birthyear: '1965',
-            Deathyear: 'none'
-        },
-        Genre: { 
-            Name: 'Drama',
-            Description: 'Stories with high stakes and many conflicts.'
-        }
-    },
-    {
-        Title: 'Inglorious Basterds',
-        Director: { 
-            Name:'Quentin Tarantino',
-            Bio: ' His films are characterized by stylized violence, extended dialogue including a pervasive use of profanity, and references to popular culture.',
-            Birthyear: '1963',
-            Deathyear: 'none'
-        },
-        Genre: { 
-            Name: 'Dark comedy',
-            Description: 'A style of comedy that makes light of subject matter that is generally considered taboo, particularly subjects that are normally considered serious or painful to discuss.'
-        }
-    },
-    {
-        Title: 'Jurrasic Park',
-        Director: { 
-            Name:'Steven Spielberg',
-            Bio: 'A style of filmmaking is based on three elements: storytelling, character development, and suspense. Spielberg knows how to tell a story in such a way that it will leave you wanting more after every scene.',
-            Birthyear: '1946',
-            Deathyear: 'none'
-        },
-        Genre: { 
-            Name:'Science fiction',
-            Description: 'A genre that speculates about alternative ways of life made possible by technological change.'
-        }
-    },
-    {
-        Title: 'The Thing',
-        Director: { 
-            Name:'John Carpenter',
-            Bio: 'An American filmmaker, actor, and composer. Although he has worked in various film genres, he is most commonly associated with horror, action, and science fiction films of the 1970s and 1980s.',
-            Birthyear: '1948',
-            Deathyear: 'none'
-        },
-        Genre: { 
-            Name: 'Horror',
-            Description: 'A genre of literature, film, and television that is meant to scare, startle, shock, and even repulse audiences.'
-        }
-    },
-    {
-        Title: 'Your Name',
-        Director: { 
-            Name:'Makoto Shinkai',
-            Bio: 'While anime and anime films often cater to the genre of fantasy and speculative fiction, his films explore the space between realism and fiction.',
-            Birthyear: '1973',
-            Deathyear: 'none'
-        },
-        Genre: { 
-            Name: 'Romance',
-            Description: 'A genre that involves a mysterious, adventurous, or spiritual story line where the focus is on a quest that involves bravery and strong values, not always a love interest.'
-
-        }
-    },
-    {
-        Title: 'Spiderman 2',
-        Director: { 
-            Name:'Sam Raimi',
-            Bio: 'His distinctive style is usually a blend of horror and comedy in varying degrees, his effective use of montages, and his camera work that you can tell is a Raimi movie at a glance.',
-            Birthyear: '1959',
-            Deathyear: 'none'
-        },
-        Genre : { 
-            Name: 'Superhero',
-            Description: 'A genre of speculative fiction examining the adventures, personalities and ethics of costumed crime fighters known as superheroes, who often possess superhuman powers and battle similarly powered criminals known as supervillains.'
-        }
-    },
-    {
-        Title: 'Nope',
-        Director: { 
-            Name:'Jordan Peele',
-            Bio: 'An American actor, comedian, and filmmaker. He is best known for his film and television work in the comedy and horror genres.',
-            Birthyear: '1979',
-            Deathyear: 'none'
-        },
-        Genre: { 
-            Name: 'Mystery', 
-            Description: 'A genre where the nature of an event remains mysterious until the end of the story.' 
-        }   
-    },
-    
-    ];
 
     app.use(express.static('public'));
 
@@ -173,128 +37,205 @@ let movies = [
     });
 
     // Movie GET requests
+  
     app.get('/movies', (req, res) => {
-        res.json(movies);
+        Movies.find()
+        .then((movies) => {
+            res.status(201).json(movies);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
     });
 
     app.get('/movies/:title', (req, res) => {
-        const { title } = req.params;
-        const movie = movies.find( movie => movie.Title === title );
-
-        if (movie) {
-            res.status(200).json(movie);
-        } else {
-            res.status(400).send('movie does not exist')
-        }
+        Movies.findOne({ Title: req.params.title })
+        .then((movies) => {
+            res.status(201).json(movies);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
     });
 
-    app.get('/movies/genre/:genreName', (req, res) => {
-        const { genreName } = req.params;
-        const genre = movies.find( movie => movie.Genre.Name === genreName ).Genre;
-
-        if (genre) {
-            res.status(200).json(genre);
-        } else {
-            res.status(400).send('genre does not exist')
-        }
+    app.get('/movies/genres/:genreName', (req, res) => {
+        Movies.findOne({ 'Genre.Name': req.params.genreName })
+        .then((movie) => {
+            res.status(201).json(movie.Genre);
+        })
+        .catch((err) => {
+            res.status(500).send('Error: ' + err);
+        });
     });
 
     app.get('/movies/directors/:directorName', (req, res) => {
-        const { directorName } = req.params;
-        const director = movies.find( movie => movie.Director.Name === directorName ).Director;
-
-        if (director) {
-            res.status(200).json(director);
-        } else {
-            res.status(400).send('director does not exist')
-        }
+        Movies.findOne({ 'Director.Name': req.params.directorName })
+        .then((movie) => {
+            res.status(201).json(movie.Director);
+        })
+        .catch((err) => {
+            res.status(500).send('Error: ' + err);
+        });
     });
 
-
-    //User GET requests
+    //GET all users
     app.get('/users', (req, res) => {
-        res.json(users);
+        Users.find()
+        .then((users) => {
+            res.status(201).json(users);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
     });
 
-    app.get('/users/:userName', (req, res) => {
-        const { userName } = req.params;
-        const user = users.find( user => user.name === userName );
-
-        if (user) {
-            res.status(200).json(user);
-        } else {
-            res.status(400).send('user does not exist')
-        }
+    //GET a user by username
+    app.get('/users/:Username', (req, res) => {
+        Users.findOne({ Username: req.params.Username })
+        .then((user) => {
+            res.json(user);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
     });
 
 
-    // POST requests CREATE
-    app.post('/users', (req,res) => {
-        const newUser = req.body;
-
-        if (newUser.name) {
-            newUser.id = uuid.v4();
-            users.push(newUser);
-            res.status(201).json(newUser)
-        } else {
-            res.status(400).send('new user need names')
-        }
+    //Add a user 
+    /* We'll expect JSON in this format
+    {
+        ID: integer,
+        Username: String,
+        Password: String,
+        Email: String,
+        Birthday: Date
+    }*/
+    app.post('/users', (req, res) => {
+        Users.findOne({ Username: req.body.Username })
+        .then((user) => {
+            if (user) {
+                return res.status(400).send(req.body.Username + ' already exists');
+            } else {
+                Users
+                .create({
+                    Username: req.body.Username,
+                    Password: req.body.Password,
+                    Email: req.body.Email,
+                    Birthday: req.body.Birthday
+                })
+                .then((user) => {res.status(201).json(user) })
+                .catch((error) => {
+                    console.error(error);
+                    res.status(500).send('Error: ' + error);
+                })
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+        });
     });
 
-    app.post('/users/:id/:movieTitle', (req, res) => {
-        const { id, movieTitle } =req.params;
-
-        let user = users.find( user => user.id == id);
-
-        if (user) {
-            user.favouriteMovies.push(movieTitle);
-            res.status(200).send('${movieTitle} has been added to user ${id}`s array');
-        } else {
-            res.status(400).send('no user exists')
-        }
-    });
+        // Add a movie to a user's list of favourites
+        app.post('/users/:Username/movies/:MovieID', (req, res) => {
+            Users.findOneAndUpdate({ Username: req.params.Username }, {
+                $push: { FavouriteMovies: req.params.MovieID },
+            },
+            { new: true } // This line makes sure that the updated document is returned
+            )
+            .then((updatedUser) => {
+                if(!updatedUser) {
+                    return res.status(400).send('Error: User was not found');
+                } else {
+                    res.json(updatedUser);
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+        });
 
     // PUT requests UPDATE
-    app.put('/users/:id', (req, res) => {
-        const { id } =req.params;
-        const updatedUser = req.body;
 
-        let user = users.find( user => user.id == id);
-
-        if (user) {
-            user.name = updatedUser.name;
-            res.status(200).json(user);
-        } else {
-            res.status(400).send('no user exists')
+    // Update a user's info, by username
+    /* We'll expect JSON in this format
+    {
+        Username: String,
+        (required)
+        Passsword : String,
+        (required)
+        Email: String,
+        (required)
+        Birthday: Date
+    }*/
+    app.put('/users/:Username', (req, res) => {
+        Users.findOneAndUpdate(
+            { Username: req.params.Username }, 
+            { $set: {
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
         }
+      },
+      { new: true }, // This line makes sure that the updated document is returned
+        )
+        .then((user) => {
+            if(!user) {
+                return res.status(400).send('Error: No user was found');
+            } else {
+                res.json(user);
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
     });
 
 
     //DELETE requests
-    app.delete('/users/:id/:movieTitle', (req, res) => {
-        const { id, movieTitle } =req.params;
 
-        let user = users.find( user => user.id == id);
-
-        if (user) {
-            user.favouriteMovies = user.favouriteMovies.filter( title => title !== movieTitle);
-            res.status(200).send('${movieTitle} has been removed from user ${id}`s array');
-        } else {
-            res.status(400).send('no user exists')
-        }
+    //Delete a movie from a users favourite movies list
+    app.delete('/users/:Username/movies/:MovieID', (req, res) => {
+        Users.findOneAndUpdate(
+            { Username: req.params.Username },
+            {
+                $pull: { FavouriteMovies: req.params.MovieID }
+            },
+            { new: true }
+            )
+        .then((updatedUser) => {
+            if(!updatedUser) {
+                res.status(400).send(req.params.MovieID + ' was not found');
+            } else {
+                res.status(200).send(req.params.movieID + ' was deleted');
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' +err);
+        });
     });
 
-    app.delete('/users/:id', (req, res) => {
-        const { id } =req.params;
-
-        let user = users.find( user => user.id == id);
-
-        if (user) {
-            users = users.filter( user => user.id != id);
-            res.status(200).send('user ${id} has been deleted');
-        } else {
-            res.status(400).send('no user exists')
-        }
+    // Delete a user by username
+    app.delete('/users/:Username', (req, res) => {
+        Users.findOneAndRemove({ Username: req.params.Username })
+        .then((user) => {
+            if (!user) {
+                res.status(400).send(req.params.Username + ' was not found');
+            } else {
+                res.status(200).send(req.params.Username + ' was deleted.');
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
     });
 
 
